5897483566224ca1dca157e2efc697f42d590874
Ramazan <43831480+ramdemi@users.noreply.github.com>
2024-11-12 01:49:12 +0300

    Update config.js
    database ismi değiştırildi

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST || 'remotemysql.com',
-    user: env.DB_USER || 'fhckf4OCq3',
-    password: env.DB_PASSWORD || 'WGPjcasxfP',
-    database: env.DB_NAME || 'fhckf4OCq3',
+    host: env.DB_HOST || '5.2.85.46',
+    user: env.DB_USER || 'periyodi_geouser',
+    password: env.DB_PASSWORD || '5W*I}=?6{-%{',
+    database: env.DB_NAME || 'periyodi_ramdem',
     waitForConnections: true,
     connectionLimit: env.DB_CONN_LIMIT || 2,
     queueLimit: 0,
